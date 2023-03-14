3de9d03851da536685f8bc2e203398a5c2486ef7
Aqil <aqil.mughal@devsinc.com>
2023-03-14 16:29:05 +0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+//from 1st may
+var firebaseConfig = {
+  apiKey: "AIzaSyD4lvKwzWrvSkb8d2yvrFbfNuuO1qFzdek",
+  authDomain: "todolist-app-87201.firebaseapp.com",
+  projectId: "todolist-app-87201",
+  storageBucket: "todolist-app-87201.appspot.com",
+  messagingSenderId: "1042447653501",
+  appId: "1:1042447653501:web:a1d4299de937cbbd15ef14",
+  measurementId: "G-HLTPL1R72W",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
